89ae9ffb7197c65e57f6bb07701d3512dd3b203a
LordNikon <100640945+wutangdev@users.noreply.github.com>
2022-07-21 16:38:31 -0700

    build command

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -2,7 +2,7 @@
 cd /home/ec2-user/server
 npm start
 npm run build
-pm2 start npm --name "yeti-static" -- start
+pm2 start npm --name "yeti-static" --interpreter bash -- start
 pm2 startup
 pm2 save
 pm2 restart all
