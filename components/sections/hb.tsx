14ece4b297e653a10acc78bc5f65136a98f9e52c
LordNikon <100640945+wutangdev@users.noreply.github.com>
2022-08-17 15:39:13 -0700

    volen updates

--- components/sections/hb.tsx
+++ components/sections/hb.tsx
@@ -19,8 +19,8 @@ function hbSection() {
                         <div className="d-flex">
                             <div className="me-2 text-success-gradiant icon-size"><img src='/images/icons/hbDiamond.svg'></img></div>
                             <div className="">
-                                <h2 className="font-weight-medium bubblegum">Networking Paradise</h2>
-                                <p className="mt-3">The Yeti Secret Society allows you to brush shoulders with elite movers and shakers at exclusive events, yacht parties, even concerts on private islands.</p>
+                                <h2 className="font-weight-medium bubblegum">Networking Redefined </h2>
+                                <p className="mt-3">The Yeti Secret Society allows you to brush shoulders with an elite community at members-only events. Our first event will be the end of the year in Las Vegas, followed by a massive party on a private island in Italy, and an all-inclusive cruise on the world's largest sailing vessel to Monaco.</p>
                             </div>
                         </div>
                     </div>
@@ -28,8 +28,17 @@ function hbSection() {
                         <div className="d-flex">
                             <div className="me-2 text-success-gradiant icon-size"><img src='/images/icons/hbChair.svg'></img></div>
                             <div className="">
-                                <h2 className="font-weight-medium bubblegum">Exclusive Yeti Lounge</h2>
-                                <p className="mt-3">The crown jewel of the Yeti Secret Society is a physical space called “The Club”, available to all holders throughout the year. A hotspot for elite networking. Only holders are given its location.</p>
+                                <h2 className="font-weight-medium bubblegum">The Yeti Lounge</h2>
+                                <p className="mt-3">The crown jewel of YSS is a physical space called “The Lounge,” available to all holders throughout the year – only Yetis are given access to this secret location.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="border-0 mb-4" data-aos="fade-up">
+                        <div className="d-flex">
+                            <div className="me-2 text-success-gradiant icon-size"><img src='/images/icons/hbChair.svg'></img></div>
+                            <div className="">
+                                <h2 className="font-weight-medium bubblegum">Intellectual Property Rights & DAO Membership</h2>
+                                <p className="mt-3">Yeti owners have full intellectual property rights of their individual NFTs, and be eligible for membership of the Yeti DAO – further information on protocols will be revealed.</p>
                             </div>
                         </div>
                     </div>
@@ -37,8 +46,8 @@ function hbSection() {
                         <div className="d-flex">
                             <div className="me-2 text-success-gradiant icon-size"><img src='/images/icons/hbDrinks.svg'></img></div>
                             <div className="">
-                                <h2 className="font-weight-medium bubblegum">Events, Events, Events..</h2>
-                                <p className="mt-3">The Yeti Secret Society has massive events, parties, even cruises on a mega yachts planned. The first event kicks off at the end of the year in Las Vegas, followed by a massive party on a private island in Italy next year, and then an all inclusive cruise on the world's largest sailing vessel to Monaco.</p>
+                                <h2 className="font-weight-medium bubblegum">Giveaways</h2>
+                                <p className="mt-3">YSS is giving away a number of Rolex watches to the public in 2022. In 2023, only YSS owners will be eligible to win three supercars and custom merch.  </p>
                             </div>
                         </div>
                     </div>
@@ -47,16 +56,16 @@ function hbSection() {
                             <div className="me-2 text-success-gradiant icon-size"><img src='/images/icons/hbPalm.svg'></img></div>
                             <div className="">
                                 <h2 className="font-weight-medium bubblegum">MMO Game and Metaverse</h2>
-                                <p className="mt-3">Along with parties and events yearly, the Yeti Secret Society is bringing its membership to the virtual world. Having partnered with Sapphire Studios to create a Massive Multiplayer Online mobile game and metaverse.</p>
+                                <p className="mt-3">Along with our annual member-only events, YSS is bringing its membership to the virtual world. We’ve partnered with a metaverse and gaming studio to create a Massive Multiplayer Online mobile game and metaverse for the Yetis.</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className='col text-center'>
+                {/* <div className='col text-center'>
                     <Zoom>
                     <img className='holder-chart' data-aos="fade-up" src='/images/charts/holderChart.png'></img>
                     </Zoom>
-                </div>
+                </div> */}
             </div>
         </div>
         </div>
